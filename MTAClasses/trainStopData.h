c76057bf7fe32854103e1db7dca801df909fc207
Norman Zhao <nzhao1996@gmail.com>
2016-02-20 21:51:38 -0500

    Finished full separation of all classes. Need to implement a working console and a search function.

---
+++ MTAClasses/trainStopData.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>;
+using namespace std;
+class trainStopData
+{
+public:
+	trainStopData::trainStopData();
+	trainStopData::trainStopData(const string& id, const string& name, double lat, double lon);
+	string trainStopData::get_id()         const;
+	string trainStopData::get_stop_name()  const;
+	double trainStopData::get_latitude()   const;
+	double trainStopData::get_longitude()  const;
+private:
+	string stop_id;
+	string stop_name;
+	double stop_lat;
+	double stop_lon;
+};
+
